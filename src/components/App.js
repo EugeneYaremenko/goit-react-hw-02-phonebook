0b166005d12897ff0c1394d8f077f77548933d1b
EugeneYaremenko <yaremenko.e.s@gmail.com>
2020-10-10 17:14:57 +0300

    add hw-02-phonebook

---
+++ src/components/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Wrapper from './Wrapper/Wrapper';
+import Section from './Section/Section';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleChangeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  handleAddContact = (text, number) => {
+    const { contacts } = this.state;
+    const dobleName = contacts.find(
+      cont => cont.name.toLowerCase() === text.toLowerCase(),
+    );
+
+    const contact = {
+      id: uuidv4(),
+      name: text,
+      number: number,
+    };
+
+    if (dobleName) {
+      alert(`${text} is already in contacts.`);
+
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, contact],
+      };
+    });
+  };
+
+  handleVisibleContact = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  handleRemoveContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId,
+        ),
+      };
+    });
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContact = this.handleVisibleContact();
+
+    return (
+      <Wrapper>
+        <Section title="Phonebook">
+          <ContactForm onAddContact={this.handleAddContact} />
+        </Section>
+
+        <Section title="Contacts">
+          <Filter
+            title="Find contacts by name"
+            value={filter}
+            onChangeFilter={this.handleChangeFilter}
+          />
+
+          {visibleContact.length > 0 && (
+            <ContactList
+              contacts={visibleContact}
+              onRemoveContact={this.handleRemoveContact}
+            />
+          )}
+        </Section>
+      </Wrapper>
+    );
+  }
+}
