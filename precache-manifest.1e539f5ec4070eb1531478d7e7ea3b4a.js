1c05be28edb2690a841f55a2ad32f137d3f24e56
EugeneYaremenko <yaremenko.e.s@gmail.com>
2020-10-10 17:10:31 +0300

    Updates

---
+++ precache-manifest.1e539f5ec4070eb1531478d7e7ea3b4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395846e36a9b41bc2364fd6ad6c558c8",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "fa018c7860b15c473ca9",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.3fe81998.chunk.css"
+  },
+  {
+    "revision": "c387b01cdab7d1e59853",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.7e0591b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.7e0591b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa018c7860b15c473ca9",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.69b24738.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
